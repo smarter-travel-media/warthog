4f11833f80e18e5d821435c492a8e32167371754
Nick Pillitteri <nickp@smartertravelmedia.com>
2014-10-01 17:47:00 -0400

    First pass at "start session" requests

--- warthog/core.py
+++ warthog/core.py
@@ -2,26 +2,88 @@
 
 """
 """
+import logging
+
+import requests
+import warthog.exc
+
+
+STATUS_ENABLED = 'enabled'
+
+STATUS_DISABLED = 'disabled'
+
+STATUS_TRANSITION = 'transition'
 
 _API_VERSION = 'v2'
 
+_ACTION_AUTHENTICATE = 'authenticate'
+
+_ACTION_ENABLE = ''
+
+_ACTION_DISABLE = ''
+
+_ACTION_STATUS = ''
+
+
+class RequestsRunner(object):
+    @staticmethod
+    def get(*args, **kwargs):
+        return requests.get(*args, **kwargs)
+
+    @staticmethod
+    def post(*args, **kwargs):
+        return requests.post(*args, **kwargs)
+
 
 class SessionStartRequest(object):
-    def __init__(self, scheme_host, username, password):
+    logger = logging.getLogger('warthog')
+
+    def __init__(self, scheme_host, username, password, runner=None):
         self._scheme_host = scheme_host
         self._username = username
         self._password = password
+        self._runner = runner if runner is not None else RequestsRunner()
 
-    def __call__(self, *args, **kwargs):
-        pass
+    @staticmethod
+    def _extract_error_message(response):
+        """
+
+        :param response:
+        :return:
+        """
+        if 'fail' == response['status']:
+            return response['err']['msg'].strip(), response['err']['code']
+        raise ValueError("Unexpected response format from authentication: {0}".format(response))
+
+    def send(self):
+        url = get_base_url(self._scheme_host)
+        params = get_base_query_params(_ACTION_AUTHENTICATE)
+        params['username'] = self._username
+        params['password'] = self._password
+
+        self.logger.debug('Making session start request to %s', url)
+        r = self._runner.get(url, params=params)
+
+        if r.status_code != requests.codes['ok']:
+            raise warthog.exc.WarthogAuthFailureError(
+                'Authentication failure. HTTP status {0}: {1}'.format(
+                    r.status_code, r.text))
+
+        json = r.json()
+        if 'session_id' not in json:
+            msg, code = self._extract_error_message(json['response'])
+            raise warthog.exc.WarthogAuthFailureError('Authentication failure', msg, code)
+        return json['session_id']
 
 
 class AuthenticatedRequest(object):
-    def __init__(self, scheme_host, session_id):
-        self._session_id = session_id
-        self._scheme_host = scheme_host
+    logger = logging.getLogger('warthog')
 
-    def __call__(self, *args, **kwargs):
+    def __init__(self, scheme_host, session_id):
+        self._scheme_host = scheme_host
+        self._session_id = session_id
+
+    def send(self):
         pass
 
 
@@ -37,3 +99,16 @@ class NodeStatusRequest(AuthenticatedRequest):
     pass
 
 
+def get_base_url(scheme_host):
+    return '/'.join([scheme_host, 'services', 'rest', _API_VERSION]) + '/'
+
+
+def get_base_query_params(action, session_id=None):
+    params = {
+        'format': 'json',
+        'method': action
+    }
+
+    if session_id is not None:
+        params['session_id'] = session_id
+    return params
---
+++ warthog/exc.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+from __future__ import print_function, division
+
+
+class WarthogError(Exception):
+    def __init__(self, msg, api_msg=None, api_code=None):
+        self.msg = msg
+        self.api_msg = api_msg
+        self.api_code = api_code
+
+    def __str__(self):
+        out = [self.msg]
+        if self.api_msg is not None:
+            out.append('API-message: {0}'.format(self.api_msg))
+        if self.api_code is not None:
+            out.append('API-code: {0}'.format(self.api_code))
+        return '. '.join(out)
+
+    def __repr__(self):
+        return '{clazz}({msg}, {api_msg}, {api_code})'.format(
+            clazz=self.__class__.__name__, msg=self.msg, api_msg=self.api_msg,
+            api_code=self.api_code)
+
+
+class WarthogAuthFailureError(WarthogError):
+    """
+    """
