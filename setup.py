22e7a1161e63964ad6f601c32140233cb124df40
Nick Pillitteri <nickp@smartertravelmedia.com>
2014-10-06 11:36:03 -0400

    Fill out setup.py with meta data

--- setup.py
+++ setup.py
@@ -7,6 +7,20 @@ import warthog
 AUTHOR = 'Smarter Travel',
 EMAIL = ''
 DESCRIPTION = 'Simple client for A10 load balancers'
+URL = 'https://warthog.readthedocs.org/'
+LICENSE = 'MIT'
+CLASSIFIERS = [
+    "Development Status :: 2 - Pre-Alpha",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: POSIX",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Topic :: System :: Installation/Setup",
+    "Topic :: System :: Software Distribution"
+]
 
 REQUIREMENTS = [
     'requests'
@@ -18,6 +32,9 @@ setup(
     author=AUTHOR,
     description=DESCRIPTION,
     author_email=EMAIL,
+    classifiers=CLASSIFIERS,
+    license=LICENSE,
+    url=URL,
     install_requires=REQUIREMENTS,
     zip_safe=True,
     packages=find_packages())
