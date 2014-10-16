c1d218ec36fdf1f875eae128d73ae77f51639c21
Nick Pillitteri <nickp@smartertravelmedia.com>
2014-10-16 17:46:05 -0400

    Bump to alpha

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ DESCRIPTION = 'Simple client for A10 load balancers'
 URL = 'https://github.com/smarter-travel-media/warthog'
 LICENSE = 'MIT'
 CLASSIFIERS = [
-    "Development Status :: 2 - Pre-Alpha",
+    "Development Status :: 3 - Alpha",
     "License :: OSI Approved :: MIT License",
     "Operating System :: POSIX",
     "Programming Language :: Python",
